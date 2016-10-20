01bf358b53f9d315cf863a27e2d2e5d1425c34bf
MichaeGon <lodnix@gmail.com>
2016-10-21 00:16:34 +0900

    fix type

--- src/TypeCheck.hs
+++ src/TypeCheck.hs
@@ -15,7 +15,7 @@ type Env = [M.Map String Type]
 type EnvState = State Env
 
 class ITypeCheck a where
-    typeCheck :: a -> EnvState (a, Type)
+    typeCheck :: a -> EnvState Type
 
 singleton :: Env
 singleton = [M.empty]
@@ -64,9 +64,8 @@ maybe' = maybe . error
 evacEnv :: EnvState a -> EnvState a
 evacEnv x = get >>= (x >>=) . ($>) . put
 
-typeCheckBlock :: (ITypeCheck a) => [a] -> EnvState ([a], Type)
-typeCheckBlock [] = return ([], Unknown)
-typeCheckBlock xs = (xs,) <$> foldl' (\acc x -> acc >> (snd <$> typeCheck x)) (return Unknown) xs
+typeCheckBlock :: (ITypeCheck a) => [a] -> EnvState Type
+typeCheckBlock = foldl' (\acc x -> acc >> typeCheck x) (return Unknown)
 
 isSubTypeOf :: Type -> Type -> Bool
 _ `isSubTypeOf` TAny = True
@@ -88,63 +87,63 @@ union x y
     | otherwise = TAny
 
 instance ITypeCheck Primary where
-    typeCheck p@(Id s) = lookupEnv s >>= maybe' ("undefined identifier: " `mappend` s) (return . (p,))
+    typeCheck (Id s) = fromMaybe (error $ "undefined identifier: " `mappend` s) <$> lookupEnv s
     typeCheck p@(DefApp prim xs) = error "undefined defapp"
     typeCheck p@(Fun xs t b) = error "undefined defapp "
     typeCheck p@(Dot prim x) = error "undefined dot"
-    typeCheck p@(Array xs) = (p,) . TArray . foldl1' union <$> mapM (fmap snd . typeCheck) xs
-    typeCheck p@(Index prim xs) = check <$> typeCheck prim <*> typeCheck xs
+    typeCheck (Array xs) = TArray . foldl' union Unknown <$> mapM typeCheck xs
+    typeCheck (Index prim xs) = check <$> typeCheck prim <*> typeCheck xs
         where
-            check (Array _, TArray arrt) (_, nt)
-                | nt `isSubTypeOf` TInt = (p, arrt)
+            check (TArray arrt) nt
+                | nt `isSubTypeOf` TInt = arrt
                 | otherwise = error $ "expect Int at index but " `mappend` show nt
-            check (_, t) _ = error $ "expect array at index but " `mappend` show t
-    typeCheck p@(Num _) = return (p, TInt)
-    typeCheck p@(Str _) = return (p, TString)
+            check t _ = error $ "expect array at index but " `mappend` show t
+    typeCheck (Num _) = return TInt
+    typeCheck (Str _) = return TString
 
 instance ITypeCheck Expr where
-    typeCheck e@(Pos p) = (e,) . snd <$> typeCheck p
-    typeCheck e@(Neg p) = check <$> typeCheck p
+    typeCheck (Pos p) = typeCheck p
+    typeCheck (Neg p) = typeCheck p
         where
-            check (_, pt)
-                | pt `isSubTypeOf` TInt = (e, pt)
+            check pt
+                | pt `isSubTypeOf` TInt = pt
                 | otherwise = error $ "expect Int at negative but: " `mappend` show pt
-    typeCheck e@(Bin l x r) = error "undefined bin"
+    typeCheck (Bin l x r) = error "undefined bin"
 
 instance ITypeCheck Stmt where
-    typeCheck s@(If c b (Just e)) = check <$> typeCheck c <*> typeCheckBlock b <*> typeCheckBlock e
+    typeCheck (If c b (Just e)) = check <$> typeCheck c <*> typeCheckBlock b <*> typeCheckBlock e
         where
-            check (_, ct) (_, bt) (_, et)
-                | ct `isSubTypeOf` TInt = (s, ct `union` bt `union` et)
+            check ct bt et
+                | ct `isSubTypeOf` TInt = ct `union` bt `union` et
                 | otherwise = error $ "expect Int at if condition but: " `mappend` show ct
-    typeCheck s@(If c b _) = check <$> typeCheck c <*> typeCheckBlock b
+    typeCheck (If c b _) = check <$> typeCheck c <*> typeCheckBlock b
         where
-            check (_, ct) (_, bt)
-                | ct `isSubTypeOf` TInt = (s, ct `union` bt)
+            check ct bt
+                | ct `isSubTypeOf` TInt = ct `union` bt
                 | otherwise = error $ "expect Int at if condition but: " `mappend` show ct
-    typeCheck s@(While c b) = check <$> typeCheck c <*> typeCheckBlock b
+    typeCheck (While c b) = check <$> typeCheck c <*> typeCheckBlock b
         where
-            check (_, ct) (_, bt)
-                | ct `isSubTypeOf` TInt = (s, ct `union` bt)
+            check ct bt
+                | ct `isSubTypeOf` TInt = ct `union` bt
                 | otherwise = error $ "expect Int at while condition but: " `mappend` show ct
-    typeCheck s@(Def name xs t b) = lookupEnv name >>= maybe check (const (error $ "duplicate definition: " `mappend` name))
+    typeCheck (Def name xs t b) = lookupEnv name >>= maybe check (const (error $ "duplicate definition: " `mappend` name))
         where
             check = error "undefined def"
 
-    typeCheck s@(Class name sc b) = lookupEnv name >>= maybe check (const (error $ "duplicate definition: " `mappend` name))
+    typeCheck (Class name sc b) = lookupEnv name >>= maybe check (const (error $ "duplicate definition: " `mappend` name))
         where
             check = error "undefined class"
 
-    typeCheck s@(Var name t e) = pop >>= check . fromJust
+    typeCheck (Var name t e) = pop >>= check . fromJust
         where
             check z = maybe dv jf $ lookup name z
                 where
                     dv = typeCheck e >>= check'
-                    check' (_, et)
+                    check' et
                         | t == Unknown = success et
                         | et `isSubTypeOf` t = success t
                         | otherwise = error $ "type mismatch at variable: " `mappend` name
                     jf = const (error $ "duplicate variable: " `mappend` name)
-                    success x = (s, x) <$ push (insert name x z)
+                    success x = x <$ push (insert name x z)
 
-    typeCheck s@(Single e) = (s,) . snd <$> typeCheck e
+    typeCheck (Single e) = typeCheck e
